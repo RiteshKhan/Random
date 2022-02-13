c00f6166f553b0c782bbf7b1c82fa51a7b3d1e46
Ritesh Khan <khanritesh28@gmail.com>
2022-02-13 10:28:59 +0530

    Add files via upload

--- pointer.cpp
+++ pointer.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 void pass_as_value(int* p){
     p = new int(7);
+    cout << "Adrs pass by val" << endl;
+    cout << p << endl;
 }
 void pass_as_ref_pointer(int*& p){
     p = new int(9);
+    cout << "Adrs pass by ref" << endl;
+    cout << p << endl;
 }
 int main(){
     int* p1 = NULL;
@@ -13,7 +17,11 @@ int main(){
     p1 = &x;
     p2 = p1;
     pass_as_value(p1);
-    pass_as_ref_pointer(p2);
+    cout << "Adrs pass by val updated p1" << endl;
+    cout << p1 << endl;
     cout << *p1 << endl;
+    pass_as_ref_pointer(p2);
+    cout << "Adrs pass by ref updated p2" << endl;
+    cout << p2 << endl;
     cout << *p2 << endl;
 }
