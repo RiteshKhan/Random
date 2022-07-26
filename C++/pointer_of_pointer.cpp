5d11b6e83c3cee3e194efa800dda14548dc9142f
Ritesh Khan <khanritesh28@gmail.com>
2022-07-26 11:24:49 +0530

    Add files via upload

---
+++ C++/pointer_of_pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 5;
+    int *p = NULL;
+    p = &x;
+
+    int **q = &p;
+    ////// The below 43 are same //////
+    cout << *q << endl;
+    cout << p << endl;
+    cout << &x << endl;
+    //////////////////////////////////
+    cout << endl;
+    
+    ///// The below 2 are same ///////
+    cout << &p << endl;
+    cout << q << endl;
+    //////////////////////////////////
+    return 0;
+}
